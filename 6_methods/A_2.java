51ec9d410e94f5ed5cf3a240d8a443350a43a6d1
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-06-23 16:30:45 +0530

    Added B_1_methods.java

---
+++ 6_methods/A_2.java
@@ -0,0 +1,34 @@
+// public class A_2 {
+//     public static void main(String[] args) {
+//         int a=5 , b=20 ;
+//         // int temp = a;
+//         // a=b; +
+
+
+//         // b=temp;
+//         // System.out.println(a+" "+b);
+
+//         //or two variable
+//         a=a+b;
+//         b=a-b;
+//         a=a-b;
+//         System.out.println(a+" "+b);
+
+//     }
+
+// }
+
+// public class A_2 {
+//     public static void change(int x){ // this x is different (pass by value ) like a=x
+//         x=10;
+//     }
+//     public static void main(String[] args) {
+//         int x=6;
+//         System.out.println(x);
+//         change(x);
+//         System.out.println(x);
+        
+               
+//     }
+
+// }
