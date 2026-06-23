f7723e28161889d887c407e632f85b7809bcc0ae
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-06-23 15:07:23 +0530

    Added A_1_basic.java

---
+++ 6_methods/A_1_basic.java
@@ -0,0 +1,43 @@
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//     int max = prices[0];
+//     for(int i=1 ; i<prices.length ; i++){
+//         if(prices[i]<max){
+//             max=prices[i];
+//         }
+//         if (prices[i-1]<prices[i]){
+//             return prices[i]-prices[i-1];
+//         }
+//     }
+//     return 0;
+        
+//     }
+// }
+
+//return 5 sirf value wapas bhejta hai, print nahi karta.
+// public class A_1_basic {
+//     public static void main(String[] args) {
+//         System.out.print(Math.pow(2,6));
+               
+//     }
+
+// }
+
+
+// import java.util.Scanner;
+// public class A_1_basic {
+//     public static void display(String x) {
+//           System.out.print(x);
+//     }
+//     public static void main(String[] args) {
+//         System.out.print("Enter  : ");
+//         Scanner sc = new Scanner(System.in);
+//         String k = sc.nextLine();   //only next() one word or letter
+//                                     //char ch = sc.next().charAt(0); SIngle Letter
+
+//         display(k);
+        
+//     }
+
+// }
+
