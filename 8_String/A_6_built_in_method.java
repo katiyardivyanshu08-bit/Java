139046d083acf38c8c506b3eeb631f3db23a90eb
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-07-02 23:45:31 +0530

    built in method add

--- 8_String/A_6_built_in_method.java
+++ 8_String/A_6_built_in_method.java
@@ -3,7 +3,9 @@ public class A_6_built_in_method {
         // String s = "rajat katiyar";
         // System.out.println(s.indexOf('a'));  // first occurance return 
         // System.out.println(s.lastIndexOf("a"));  // if not return -1
+
         // System.out.println(s.contains("kat"));  // true
+
         // System.out.println(s.startsWith("rat"));
         // lexographically
          // rajat rajt  -> rajat < rajt  beacause a come fist
@@ -21,6 +23,8 @@ public class A_6_built_in_method {
         // System.out.println(10+s+b+20);
         // System.out.println(s+b+(10+20));
         // System.out.println('A'+2+"rajat");
+
+
 
         // int to string
         // int x=10;
@@ -47,11 +51,38 @@ public class A_6_built_in_method {
         // System.out.println(arr[0]);
         // for(char ch : arr) System.out.print(ch);
 
+        // character array to string
+        // char [] ch = {'j','a','v','a'};
+        // // String str = String.valueOf(ch);
+        // String str = new String(ch);
+        // System.out.println(str);
+
+        //string to string array
+        // s=s.trim();  // remove starting and ending spaces
+        // String str = "I     am";
+        // String[] arr = str.split("\\s+"); // white space remover  (+) means one or more
+
+        // String str = "My name is rajat";
+        // String [] arr = str.split(" ");  // ""m  or  " "My
+        // String str = "Apple,Mango,Banana";
+        // String[] arr = str.split(",");
+        // for(String s : arr) System.out.println(s);
+
+        //String array to string
+        // String [] arr = {"My","name","is","Divyanshu"};
+        // String str = String.join(" ", arr); //My name is Divyanshu
+        // String str = String.join(",", arr); //My,name,is,Divyanshu 
+        // System.out.println(str);
+
+    
+        // System.out.println(ch.getClass().getName()); //type
+
         //subString of string
-        String s= "Jaishankar";
-        System.out.println(s.substring(0));
-        System.out.println(s.substring(3));
-        System.out.println(s.substring(3,s.length())); // before n-1
+        // String s= "Jaishankar";
+        // System.out.println(s.substring(0));
+        // System.out.println(s.substring(3));
+        // System.out.println(s.substring(3,s.length())); // before n-1
+        
 
     }
 }
