4a4153950ec1f5a3517fefeea198a99ef9413843
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-07-03 01:07:11 +0530

    reverse word

--- 8_String/A_10_1_reverse_word_in_str_remove_spa.java
+++ 8_String/A_10_1_reverse_word_in_str_remove_spa.java
@@ -1,6 +1,21 @@
 public class A_10_1_reverse_word_in_str_remove_spa {
     public static void main(String[] args) {
         String  s = "My name is rajat";
+
+        String [] arr = s.trim().split("\\s+");
+        int n=arr.length;
         
+        for(int i=0 ; i<n ; i++){
+            String rev ="";
+            for(int j=arr[i].length()-1; j>=0;j--){
+                String q = arr[i];
+                rev+=q.charAt(j);
+            }
+            arr[i]=rev;
+        }
+        // for(String x : arr) System.out.print(x+" ");
+        // System.out.println();
+        System.out.println(String.join(" ", arr));
+
     }
 }
