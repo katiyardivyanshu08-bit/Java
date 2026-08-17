6997545abf932658163e35f5cbce16b2a4725246
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-08-17 15:05:30 +0530

    Clean up commented code in A_4_middile.java
    Removed commented-out code related to finding the middle of a linked list.

--- 15_linkedLIst_1/A_4_middile.java
+++ 15_linkedLIst_1/A_4_middile.java
@@ -11,27 +11,15 @@
 //             slow = slow.next;
 //             fast = fast.next.next;
 //         }
-//         ListNode middle = slow;
         
 //        // left Middle
 //         ListNode slow = head;
 //         ListNode fast = head;
-//         ListNode left = null;
-//         while(fast != null && fast.next != null){
-//             left = slow;
+//         while(fast.next!= null && fast.next.next != null){
 //             slow = slow.next;
 //             fast = fast.next.next;
 //         }
-        
-//         // right middle
-//         ListNode slow = head;
-//         ListNode fast = head;
-//         while(fast != null && fast.next != null){
-//              slow = slow.next;
-//             fast = fast.next.next;
-//         }
-//          ListNode right = slow.next; 
-
+         
         // // Not a ONE PASS solution
         // int length = 0;
         // Node temp = head;
