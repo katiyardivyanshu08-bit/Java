95873652f36eb3f721aac0ef44609775176cd81e
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-08-18 14:47:13 +0530

    Document sorting algorithms and complexities
    Added comments detailing sorting algorithms and their complexities.

--- 9_bubble_selection_insertion_sort/A_1_bubble_sort.java
+++ 9_bubble_selection_insertion_sort/A_1_bubble_sort.java
@@ -1,3 +1,21 @@
+// Sorting	            Main technique used
+// Bubble Sort	        Repeated comparison + swapping
+// Selection Sort	    Find minimum/maximum + selection
+// Insertion Sort	    Incremental insertion
+// Merge Sort	        Recursion + Divide & Conquer
+// Quick Sort	        Recursion + Divide & Conquer + Partitioning
+// Cyclic Sort	        Index placement
+
+// SORTING ALGORITHMS – TIME & SPACE COMPLEXITY
+// Algorithm          Best Case      Average Case    Worst Case     Space
+// -----------------------------------------------------------------------
+// Bubble Sort        O(n)           O(n²)           O(n²)          O(1)
+// Selection Sort     O(n²)          O(n²)           O(n²)          O(1)
+// Insertion Sort     O(n)           O(n²)           O(n²)          O(1)
+// Merge Sort         O(n log n)     O(n log n)      O(n log n)     O(n)
+// Quick Sort         O(n log n)     O(n log n)      O(n²)          O(log n)*
+// Cyclic Sort        O(n)           O(n)            O(n)           O(1)
+
 public class A_1_bubble_sort {
     public static void main(String[] args) {
         int [] a = {1,5,8,-6,4,-8,4,8,-2,3};
