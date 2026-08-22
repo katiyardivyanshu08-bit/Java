9e4167ae0da22bac9631d289fe8799a798c91dce
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-08-22 09:53:48 +0530

    Refactor insertion sort algorithm
    Refactored insertion sort implementation for clarity and efficiency.

--- 9_bubble_selection_insertion_sort/A_3_insertion_sort.java
+++ 9_bubble_selection_insertion_sort/A_3_insertion_sort.java
@@ -1,5 +1,6 @@
 public class A_3_insertion_sort {
     public static void main(String[] args) {
+        // inplace sorting shifting the value (copy paste)
         // insertion sort -> divide array in two parts sort and unsorted In each pass, pick 
         // the first element from the unsorted part and insert it into its correct position
         //  in the sorted part.
@@ -7,15 +8,26 @@ public class A_3_insertion_sort {
         // best case -> O(n) otherwise O(n2)
         // stable
         int[] arr = {4, 1, 7, 3, 9, 2, 0, 8};
-         for (int i = 1; i < arr.length; i++) {
-            int j = i;
-            while (j > 0 && arr[j] < arr[j - 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = temp;
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
                 j--;
             }
+            arr[j+1]=key;
         }
+        
+        //  for (int i = 1; i < arr.length; i++) {
+        //     int j = i;
+        //     while (j > 0 && arr[j] < arr[j - 1]) {
+        //         int temp = arr[j];
+        //         arr[j] = arr[j - 1];
+        //         arr[j - 1] = temp;
+        //         j--;
+        //     }
+        // }
         for (int ele : arr) {
             System.out.print(ele + " ");
         }
