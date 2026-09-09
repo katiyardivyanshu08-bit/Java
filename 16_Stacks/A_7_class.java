74e1cb8697d3b32985c955283c278c178a6e1839
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-09-09 15:22:15 +0530

    Refactor Stack class and improve method implementations
    Refactored Stack class methods for better readability and fixed push method logic.

--- 16_Stacks/A_7_class.java
+++ 16_Stacks/A_7_class.java
@@ -1,25 +1,56 @@
-class Stack{
-  private int[] arr ;
-  private int top;
-  private int capacity;
+class Stack {
+    private int[] arr;
+    private int top;
+    private int capacity;
 
-  Stack(int capacity){
-    this.capacity=capacity;
-    arr = new int[capacity];
-  }
-  public void push(int val){
-    if(top==capacity){
-      System.out.println("Stack overflow")
-        return ;
+    Stack(int capacity){
+        this.capacity = capacity;
+        top = -1;
+        arr = new int[capacity];
     }
-    arr[top++]=val;
-  }
-  int pop(){
-    if(top==-1){
-      System.out.println("Stack underflow");
-      return -1;
+
+    void push(int value){
+        if(top == capacity-1){
+            System.out.println("Stack overflow");
+            return;
+        }
+        arr[++top] = value; 
     }
-    return arr[top--];
-  }
-  
+
+    int pop() {
+        if (top == -1) {
+            System.out.println("Stack underflow");
+            return -1;
+        }
+        return arr[top--];
+    }
+
+    int peek() {
+        if (top == -1){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return arr[top];
+    }
+
+    int size() {
+        return top+1;
+    }
+
+    int cap(){
+        return capacity;
+    }
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Stack s1 = new Stack(5);
+        s1.push(67);
+        s1.push(56);
+        s1.push(46);
+        s1.push(156);
+        System.out.println(s1.peek());
+        System.out.println(s1.pop());
+    }
 }
