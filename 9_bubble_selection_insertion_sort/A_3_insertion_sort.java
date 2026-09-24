1ca46822d1792716e3eeecffd5e28958e54399a7
Divyanshu Katiyar <katiyardivyanshu08@gmail.com>
2026-09-25 01:16:07 +0530

    Clean up commented code in insertion sort
    Removed commented-out bubble sort implementation.

--- 9_bubble_selection_insertion_sort/A_3_insertion_sort.java
+++ 9_bubble_selection_insertion_sort/A_3_insertion_sort.java
@@ -18,16 +18,6 @@ public class A_3_insertion_sort {
             }
             arr[j+1]=key;
         }
-        
-        //  for (int i = 1; i < arr.length; i++) {
-        //     int j = i;
-        //     while (j > 0 && arr[j] < arr[j - 1]) {
-        //         int temp = arr[j];
-        //         arr[j] = arr[j - 1];
-        //         arr[j - 1] = temp;
-        //         j--;
-        //     }
-        // }
         for (int ele : arr) {
             System.out.print(ele + " ");
         }
